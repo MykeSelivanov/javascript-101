2a7b7f5f9cd5c052b6de70e7229eaf460db7ba64
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-24 22:04:58 -0500

    context binding

--- js-es6+/02-arrow-functions/02-context-binding.js
+++ js-es6+/02-arrow-functions/02-context-binding.js
@@ -21,3 +21,57 @@
 // In constructor functions and prototype extensions, it does not make sense to use fat arrows.
 
 // Examples
+
+// Context binding is usually performed in one of the following two ways:
+// by defining a self = this variable,
+// by using the bind function.
+
+// ES5 with self
+var Ball = function( x, y, vx, vy ) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.dt = 25; // 1000/25 = 40 frames per second
+    var self = this;
+    setInterval( function() { 
+        self.x += vx;  
+        self.y += vy;
+        console.log( self.x, self.y );
+    }, this.dt );
+}
+
+var ball = new Ball( 0, 0, 1, 2 );
+
+// ES5 with .bind()
+// The bind method binds the context of the setInterval function argument to this.
+var Ball2 = function( x, y, vx, vy ) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.dt = 25; // 1000/25 = 40 frames per second
+    setInterval( function() { 
+        this.x += vx;  
+        this.y += vy;
+        console.log( this.x, this.y );
+    }.bind( this ), this.dt );
+}
+
+// ES6
+// In ES6, arrow functions come with automatic context binding. The lexical value of this 
+// isn’t shadowed by the scope of the arrow function. Therefore, you save yourself thinking about context binding.
+var BallES6 = function( x, y, vx, vy ) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.dt = 25; // 1000/25 = 40 frames per second
+    setInterval( () => { 
+        this.x += vx;  
+        this.y += vy;
+        console.log( this.x, this.y );
+    }, this.dt );
+}
+
+b = new BallES6( 0, 0, 1, 1 );
