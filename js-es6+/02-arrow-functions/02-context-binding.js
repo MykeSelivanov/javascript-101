ab1da883a482718720979567fdd212af46368b62
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-25 14:31:34 -0500

    context binding

--- js-es6+/02-arrow-functions/02-context-binding.js
+++ js-es6+/02-arrow-functions/02-context-binding.js
@@ -75,3 +75,21 @@ var BallES6 = function( x, y, vx, vy ) {
 }
 
 b = new BallES6( 0, 0, 1, 1 );
+
+// Ex
+// Use arrow function wherever it makes sense
+// In constructor functions and prototype extensions, it does not make sense to use fat arrows.
+var Entity = function( name, delay ) { 
+    this.name = name;
+    this.delay = delay;
+  };
+  Entity.prototype.greet = function() {
+    setTimeout(() => {
+      console.log( 'Hi, I am ' + this.name );
+    }, this.delay );
+  };
+  
+  var java = new Entity( 'Java', 5000 );
+  var cpp = new Entity( 'C++', 30 );
+  java.greet();
+  cpp.greet();
