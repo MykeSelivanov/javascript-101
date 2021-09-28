ae67801fb8076f70a5eab7d892a8ab08c9e379c3
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-27 23:50:06 -0500

    inheritance js

---
+++ js-es6+/04-classes/01-prototypal-inheritance-ES5.js
@@ -0,0 +1,4 @@
+// The concepts of prototypes and prototypal inheritance in ES5 are hard to understand for 
+// many developers transitioning from another programming language to JavaScript.
+
+// ES6 classes introduce syntactic sugar to make prototypes look like classical inheritance.
