994f21e991d897274ca38c54908cf07ec79a5101
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-28 23:44:27 -0500

    inheritance ES6

--- js-es6+/04-classes/02-inheritance-ES6.js
+++ js-es6+/04-classes/02-inheritance-ES6.js
@@ -6,6 +6,12 @@
 // Your code becomes more readable, when you capitalize class names, and start object names and method names 
 // with a lower case letter. For instance, Person should be a class, and person should be an object.
 
+// The extends keyword defines the is-a relationship between Shape and Rectangle. All instances of Rectangle are also instances of Shape.
+// The constructor method runs when you instantiate a class. You can call the constructor method of your parent class with super (more on super later).
+// Methods can be defined inside classes. All objects can call methods of their class and all classes that are higher in the inheritance chain.
+// Instantiation works in the same way as the instantiation of an ES5 constructor function.
+// The methods are written using the concise method syntax. 
+
 class Shape {
     constructor( color ) {
         this.color = color;
