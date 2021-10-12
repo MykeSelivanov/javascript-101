a1a38a37d0282d51b358d75b01ab72968a820144
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-11 23:45:13 -0500

    shadowing

---
+++ js-es6+/04-classes/04-shadowing.js
@@ -0,0 +1,5 @@
+// Methods of the parent class can be redefined in the child class. This redefinition of the function using the same name 
+// is called shadowing.
+
+// The child class SuperUser has redefined the function hasAccess
+
