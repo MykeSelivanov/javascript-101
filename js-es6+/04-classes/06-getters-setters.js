6803f7afafd11b04f6c5968449f92a8fae73eff3
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-12 19:56:24 -0500

    getters setters

--- js-es6+/04-classes/06-getters-setters.js
+++ js-es6+/04-classes/06-getters-setters.js
@@ -10,4 +10,57 @@
 
 // Easier debugging: Just add debugging commands or breakpoints to a setter, and you will know what caused a value to change.
 
-class Rectangle 
+class Square {
+    constructor(width) { this.width = width; }
+    get height() {
+        console.log('get height');
+        return this.width;
+    }
+    set height(h) {
+        console.log('set height', h);
+        this.width = h;
+    }
+    get area() {
+        console.log('get area');
+        return this.width * this.height;
+    }
+}
+
+// Width and height can be used as regular properties of a Square object, 
+// and the two values are kept in sync using the height getter and setter.
+
+let square = new Square(5);
+let print;
+
+print = square.width;
+console.log(print + '\n');
+//> 5
+
+print = square.height;
+console.log(print + '\n');
+//> get height
+//> 5
+
+print = square.height = 6;
+console.log(print + '\n');
+//> set height 6
+//> 6
+
+print = square.width;
+console.log(print + '\n');
+//> 6
+
+print = square.area;
+console.log(print + '\n');
+//> get area
+//> get height
+//> 36
+
+print = square.width = 4;
+console.log(print + '\n');
+//> 4
+
+print = square.height;
+console.log(print + '\n');
+//> get height
+//> 4
