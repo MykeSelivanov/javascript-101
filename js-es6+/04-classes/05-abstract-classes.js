051f7cc770d9b876755de68520f65d94a95a30f1
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-12 19:42:20 -0500

    abstract classes

--- js-es6+/04-classes/05-abstract-classes.js
+++ js-es6+/04-classes/05-abstract-classes.js
@@ -8,3 +8,16 @@
 
 // If you want to make ChartView abstract, do the following:
 
+class ChartView {
+    constructor( /* ... */) {
+        if (new.target === ChartView) {
+            throw new Error(
+                'Abstract class ChartView cannot be instantiated.');
+        }
+        // ...
+    }
+    // ...
+}
+
+// The built-in property new.target contains a reference to the class written next to the new keyword during instantiation. 
+// This is the class whose constructor was first called in the inheritance chain.
