607b652de0a722ea11883b083f8bf404f338e9a3
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-14 17:38:06 -0500

    destructuring

---
+++ js-es6+/05-Destructuring/01-object-property.js
@@ -0,0 +1,10 @@
+// ES5
+var language = 'Markdown';
+var extension = 'md';
+var fileName = 'Destructuring';
+
+var file = {
+    language: language,
+    extension: extension,
+    fileName: fileName
+};
