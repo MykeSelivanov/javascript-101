6b13cc49fad62185ba40e4d0d0659ad1a128a06f
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-13 23:48:36 -0500

    static methods

--- js-es6+/04-classes/07-static-methods.js
+++ js-es6+/04-classes/07-static-methods.js
@@ -1,2 +1,11 @@
 // Static methods are operations defined on a class. 
 // These methods can only be referenced from the class itself, not from objects.
+
+class C {
+    static create() { return new C(); }
+    constructor()   { console.log( 'Accessing constructor from the class'); }
+}
+
+var c = C.create();
+//> constructor - ''Accessing constructor from the class'
+c.create(); //this will give an error
