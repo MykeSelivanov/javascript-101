b965b74859285c8b9e26e2f7547a671a44dde937
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-16 15:23:09 -0500

    destructuring

---
+++ js-es6+/05-destructuring/03-deep-destructuring.js
@@ -0,0 +1,2 @@
+// Destructuring objects and arrays in any depth is possible. We can also use default values. 
+// Objects or arrays that don’t exist on the right become assigned to undefined on the left.
