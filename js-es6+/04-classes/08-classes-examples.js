85a8e9298ebe2774ba3fc161638f03c44b61e47a
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-15 21:16:57 -0500

    classes

---
+++ js-es6+/04-classes/08-classes-examples.js
@@ -0,0 +1,28 @@
+// Create a PlayerCharacter and a NonPlayerCharacter with a common ancestor Character. 
+// The characters are located in a 10x10 game field. All characters appear at a random location. 
+// Create the three classes, and make sure you can query where each character is.
+
+class Character {
+    constructor( id, name, x, y ) {
+        this.id = id;
+        this.name = name;
+        this.x = x;
+        this.y = y;
+  }
+
+  get position() {
+    return { x: this.x, y: this.y };
+  }
+}
+class PlayerCharacter extends Character { }
+class NonPlayerCharacter extends Character { }
+
+function createPlayer( id, name ) {
+ let x = Math.floor( Math.random() * 10 ), y = Math.floor( Math.random() * 10 );
+    return new PlayerCharacter( id, name, x, y );
+}
+
+function createNonPlayer( id, name ) {
+ let x = Math.floor( Math.random() * 10 ), y = Math.floor( Math.random() * 10 );
+ return new NonPlayerCharacter( id, name, x, y );
+}
