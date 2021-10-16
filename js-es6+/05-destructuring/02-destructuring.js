54ab51f1ec52ab54f88cbd9112a50669854c40b6
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-15 21:28:46 -0500

    destructuring

--- js-es6+/05-destructuring/02-destructuring.js
+++ js-es6+/05-destructuring/02-destructuring.js
@@ -23,3 +23,15 @@ let nextLesson = user.nextLesson;
 
 // Make sure you use the let keyword to initialize new variables. 
 // You can destructure an object or an array only if all the variables inside have been declared.
+
+// Destructuring is right-associative, i.e., it is evaluated from right to left. 
+// L = M = R becomes L = R, which in turn becomes R after evaluation. 
+// The side effect is that in M and L, variable assignments may take place on any depth.
+
+let user2 = {email, nextLesson} = user;
+console.log( user2 === user, user2.name ); // true, Ashley
+
+// In the above example, {email, nextLesson} = user is evaluated. 
+// The side effect of the evaluation is that email and nextLesson are assigned to "ashley@ilovees2015.net" and 10 respectively. 
+// The value of the expression is user. Then user2 = user is evaluated, creating another handle 
+// (or call it reference or pointer depending on your taste) for the object accessible via user.
