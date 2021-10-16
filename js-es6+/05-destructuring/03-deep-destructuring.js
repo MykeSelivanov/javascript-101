f468a74f52000c5f97c028791d05159550cd9040
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-16 15:23:50 -0500

    destructuring

--- js-es6+/05-destructuring/03-deep-destructuring.js
+++ js-es6+/05-destructuring/03-deep-destructuring.js
@@ -1,2 +1,22 @@
 // Destructuring objects and arrays in any depth is possible. We can also use default values. 
 // Objects or arrays that don’t exist on the right become assigned to undefined on the left.
+
+let user = {
+    name: 'Ashley',
+    email: 'ashley@ilovees2015.net',
+    lessonsSeen: [2, 5, 6, 7, 9],
+    nextLesson: 10
+};
+
+let {
+    lessonsSeen: [
+        first,
+        second,
+        third,
+        fourth,
+        fifth,
+        sixth = null,
+        seventh
+    ],
+    nextLesson: eighth
+} = user;
