2282c7e718bf119b13cec06bf787faf7463b1e4b
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-17 18:56:48 -0500

    destructuring

--- js-es6+/05-destructuring/04-destructuring-examples.js
+++ js-es6+/05-destructuring/04-destructuring-examples.js
@@ -22,3 +22,27 @@ function fib(n) {
 // Return the value in a new variable called A_2.
 let x = { A: ['t', 'e', 's', 't'] };
 let { A: [, , A_2] } = x;
+// You don’t have to provide variable names to match A[0], A[1], or A[3]. For A[3], 
+// you don’t even need to create a comma, symbolizing that A[3] exists. 
+// Similarly, adding two commas after A_2 does not make a difference either, as in JavaScript, 
+// indexing outside the bounds of an array gives us undefined. Note that A was not created as a variable in the expression. 
+// You cannot assign the name of a variable and destructure its contents at the same time.
+
+
+// Suppose the following configuration object of a financial chart is given:
+let config = {
+    chartType: 0,
+    bullColor: 'green',
+    bearColor: 'red',
+    days: 30
+};
+// Complete the function signature below such that the function may be called with any config objects 
+// (null and undefined are not allowed as inputs). If any of the four keys are missing, substitute their default values.
+//  The default values are the same as in the example configuration object.
+function drawChart(data, { chartType = 0,
+    bullColor = 'green',
+    bearColor = 'red',
+    days = 30 }) {
+    // funciton body will be here
+    return { chartType, bullColor, bearColor, days };
+};
