b322e9ae4f7b875c5b19badac898f9122acd8b9b
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-17 18:13:00 -0500

    destructuring

--- js-es6+/05-destructuring/04-destructuring-examples.js
+++ js-es6+/05-destructuring/04-destructuring-examples.js
@@ -3,3 +3,22 @@ let text1 = 'swap';
 let text2 = 'me';
 [text1, text2] = [text2, text1];
 
+// Complete the function that calculates the nth fibonacci number in the sequence with one destructuring assignment
+function fib(n) {
+    let fibCurrent = 1;
+    let fibLast = 0;
+
+    if (n < 0) return NaN;
+    if (n <= 1) return n;
+
+    for (let fibIndex = 1; fibIndex < n; ++fibIndex) {
+        [fibCurrent, fibLast] = [fibCurrent + fibLast, fibCurrent];
+    }
+
+    return fibCurrent;
+}
+
+// Create one destructuring expression that declares exactly one variable to retrieve x.A[2]. 
+// Return the value in a new variable called A_2.
+let x = { A: ['t', 'e', 's', 't'] };
+let { A: [, , A_2] } = x;
