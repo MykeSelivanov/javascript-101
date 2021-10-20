a8f98cbfce7b72a0c773bb737cdae65da511a68d
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-20 18:21:32 -0500

    mixins

--- js-es6+/07-objects-es6/02-mixins.js
+++ js-es6+/07-objects-es6/02-mixins.js
@@ -1,3 +1,10 @@
+// Heated debates of composition over inheritance made mixins appear to be the winner construct for composing objects. 
+// Therefore, libraries such as UnderscoreJs and LoDash created support for this construct with their methods _.extend or _.mixin.
+
+// In ES6, Object.assign does the same thing as _.extend or _.mixin.
+
+
+
 let horse = {
     horseName: 'QuickBucks',
     toString: function () {
