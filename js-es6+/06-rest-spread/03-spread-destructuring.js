752c99f5d2e69e6a9425e755decb998ffa8ba57c
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-19 21:23:23 -0500

    destructuring

--- js-es6+/06-rest-spread/03-spread-destructuring.js
+++ js-es6+/06-rest-spread/03-spread-destructuring.js
@@ -9,3 +9,11 @@ let notgood = 'not good'.split('');
 let [, , , , , , , , , , , , , , , , ...empty] = notgood;
 console.log(empty);
 
+// Just like the rest parameter in functions, using ... on the left of a destructuring expression creates 
+// a match for all the remaining elements of the array:
+[, ...A] = [1, 2, 3, 4]
+// A becomes [2,3,4]
+
+// Like the rest parameter in functions, on the left side of a destructuring assignment, 
+// we are only allowed to use the rest parameter as the last element of an array.
+// [...A,] = [1, 2] // => syntax error
