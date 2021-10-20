4248dcc130610ee8cdd49b85f236781b389b823c
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-19 21:21:36 -0500

    destructuring

--- js-es6+/06-rest-spread/03-spread-destructuring.js
+++ js-es6+/06-rest-spread/03-spread-destructuring.js
@@ -3,3 +3,9 @@ let notgood = 'not good'.split('');
 let [, , , , ...good] = notgood;
 console.log(good);
 // ["g", "o", "o", "d"]
+
+// If there are no elements left, the result of a destructuring assignment involving a spread operator is [].
+let notgood = 'not good'.split('');
+let [, , , , , , , , , , , , , , , , ...empty] = notgood;
+console.log(empty);
+
