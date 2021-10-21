cc4a00ff72deac53379ae9204702139ed65dd141
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-21 18:59:19 -0500

    destructuring

--- js-es6+/06-rest-spread/04-destructuring-examples.js
+++ js-es6+/06-rest-spread/04-destructuring-examples.js
@@ -1,6 +1,15 @@
+// Ex1
 // Make a shallow copy of an array of any length in one destructuring assignment
 let originalArray = [1, 2, 3, 4, 5];
 console.log(`originalArray`, originalArray);
 let [...clonedArray] = originalArray;
 console.log(`clonedArray`, clonedArray);
 
+// Ex2
+// An array of five vectors of ['1', '2', '3', '4', '5'] is printed out as a table.
+// The function f creates the array ['1', '2', '3', '4', '5']. In f().map( f ), only the length of f() matters, 
+// as the values are thrown away by the map function. Each element of the f() array is mapped to the array 
+// ['1', '2', '3', '4', '5'], making a 2 dimensional array of vectors ['1', '2', '3', '4', '5'].
+let f = () => [..."12345"];
+let A = f().map( f );
+console.log( A );
