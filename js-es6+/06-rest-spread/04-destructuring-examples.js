3ed3a1a29e534ced8d063dd805ba80608a19958c
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-21 19:02:08 -0500

    destructuring

--- js-es6+/06-rest-spread/04-destructuring-examples.js
+++ js-es6+/06-rest-spread/04-destructuring-examples.js
@@ -11,5 +11,11 @@ console.log(`clonedArray`, clonedArray);
 // as the values are thrown away by the map function. Each element of the f() array is mapped to the array 
 // ['1', '2', '3', '4', '5'], making a 2 dimensional array of vectors ['1', '2', '3', '4', '5'].
 let f = () => [..."12345"];
-let A = f().map( f );
-console.log( A );
+let A = f().map(f);
+console.log(A);
+
+// Ex3
+// Create an 10x10 matrix of null values
+let nullArray = () => new Array(10).fill(null);
+let nullMatrix = nullArray().map(nullArray);
+console.log(nullMatrix);
