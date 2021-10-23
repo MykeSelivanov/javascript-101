edb2a1b5ac7d69d6e80bf0b8b8c4e3a4a69286e2
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-23 12:11:01 -0500

    shorthand

--- js-es6+/07-objects-es6/03-creating-destructuring-shorthand.js
+++ js-es6+/07-objects-es6/03-creating-destructuring-shorthand.js
@@ -5,3 +5,9 @@ let shape = { shapeName, a, b, id: 0 };
 
 console.log( shape );
 // { shapeName: "Rectangle", a: 5, b: 3, id: 0 }
+
+// It is possible to use this shorthand in destructuring assignments for the purpose of creating new fields:
+let { x, y } = { x: 3, y: 4, z: 2 };
+
+console.log( y, typeof y );
+// 4 "number"
