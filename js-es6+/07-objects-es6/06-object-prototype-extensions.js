247bf9f1852604fb1b2d5b2b74fe19773bd928ea
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-24 16:37:01 -0500

    object prototype extensions

---
+++ js-es6+/07-objects-es6/06-object-prototype-extensions.js
@@ -0,0 +1,4 @@
+// The Object API has been extended with two methods to get and set the prototype of an object:
+
+// - Object.getPrototypeOf(o) returns the prototype of o
+// - Object.setPrototypeOf( o, proto ) sets the prototype of o to proto
