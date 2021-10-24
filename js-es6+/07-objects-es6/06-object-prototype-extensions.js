e77230679a673aa72d505bc1c8c39e61df261a7b
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-24 16:38:04 -0500

    object prototype extensions

--- js-es6+/07-objects-es6/06-object-prototype-extensions.js
+++ js-es6+/07-objects-es6/06-object-prototype-extensions.js
@@ -2,3 +2,31 @@
 
 // - Object.getPrototypeOf(o) returns the prototype of o
 // - Object.setPrototypeOf( o, proto ) sets the prototype of o to proto
+
+let proto = {
+    whoami() { console.log('I am proto'); }
+};
+
+let obj = {
+    whoami() {
+        super.whoami();
+        console.log('I am obj');
+    }
+};
+
+console.log(Object.getPrototypeOf(obj));
+//{}
+
+Object.setPrototypeOf(obj, proto);
+
+obj.whoami();
+// I am proto
+// I am obj
+
+// The following points are worth noting:
+
+// The prototype of an object is Object by default.
+
+// Object.setPrototypeOf can change this prototype to any object
+
+// The super call saves some typing. Without super, we would have to write the following:
