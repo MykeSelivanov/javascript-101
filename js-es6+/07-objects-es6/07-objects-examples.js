434ebe8e51a1bab3feb9883bac186dd8c4707cdc
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-25 23:26:48 -0500

    objects

--- js-es6+/07-objects-es6/07-objects-examples.js
+++ js-es6+/07-objects-es6/07-objects-examples.js
@@ -10,3 +10,11 @@ let newBaskets = baskets.map(item => Object.assign(
     { firstName: '-', basketValue: 0 },
     item
 ));
+
+// Explanation
+// When it comes to transforming the value of an array, we usually use the map method.
+// The shortest way of writing a map function is through using the arrow syntax. Inside the map, 
+// we have to fill in the default values in item. If a key is already given in item, it takes precedence. 
+// We will use Object.assign. For each element of the basket array, we will create an object of default values, 
+// and we extend it with the element of the array, mixing in all the properties. If a key exists in both objects, 
+// the value in item is kept.
