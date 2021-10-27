8e97287e9fffc3866671f9c5714438c2fb18d419
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-26 23:10:07 -0500

    stacks

---
+++ js-es6+/08-function-features-es6/01-stacks.js
@@ -0,0 +1,8 @@
+// There are two types of memory available to you: the stack and the heap. 
+// The heap is used for dynamic memory allocation, while the stack is used for static memory allocation. 
+// Accessing the stack is very fast, but the size of the stack is fixed.
+
+// A stack frame is created for the global scope. Then, for each function call, another stack frame is added to the top. 
+// These frames stack on top of each other.
+
+// When executing JavaScript code, you get a stack with limited size to work with.
