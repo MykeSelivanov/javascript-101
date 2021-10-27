6b4a3eeee6529eddac344b283e21239c64349db0
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-26 23:13:12 -0500

    stacks

--- js-es6+/08-function-features-es6/01-stacks.js
+++ js-es6+/08-function-features-es6/01-stacks.js
@@ -6,3 +6,25 @@
 // These frames stack on top of each other.
 
 // When executing JavaScript code, you get a stack with limited size to work with.
+
+// Regardless of the browser, iterating from zero to a million in a for loop is an easy task.
+console.time('for loop');
+let sum = 0;
+for (let i = 0; i < 1000000; ++i) {
+    sum += i;
+}
+console.timeEnd('for loop');
+// for loop: 18.749ms
+
+// Doing the same iteration as a recursive call is an arduous task that most browsers are not able to solve 
+// because of the stack limit. Execution is fast, but the stack limit will not make it possible to execute a million calls.
+function sumToN(n) {
+    if (n <= 1) return n;
+    return n + sumToN(n - 1);
+};
+
+console.time('recursion');
+console.log(sumToN(1000000));
+console.timeEnd('recursion');
+// RangeError: Maximum call stack size exceeded
+
