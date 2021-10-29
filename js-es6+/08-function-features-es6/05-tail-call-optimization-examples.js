de82b2a9c7e83a4a4374c7e37197f957f84c7880
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-29 18:57:28 -0500

    tail call optimization

--- js-es6+/08-function-features-es6/05-tail-call-optimization-examples.js
+++ js-es6+/08-function-features-es6/05-tail-call-optimization-examples.js
@@ -1,3 +1,17 @@
 // Ex1
 // Implement a stack in ES6. In addition to a constructor, it should have a push(), pop(), 
 // and a len() function that would return the number of elements in the stack.
+class Stack {
+    constructor() {
+        this._elements = [];
+    }
+    get len() {
+        return this._elements.length;
+    }
+    push(element) {
+        this._elements.push(element);
+    }
+    pop() {
+        return this._elements.pop();
+    }
+}
