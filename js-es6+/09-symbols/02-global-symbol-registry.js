bbc16351259ba1f648081cd9352efa46704c4646
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-31 15:44:10 -0500

    global symbol registry

---
+++ js-es6+/09-symbols/02-global-symbol-registry.js
@@ -0,0 +1,3 @@
+// ES6 has a global resource for creating symbols: the symbol registry. 
+// The symbol registry provides us with a one - to - one relationship between strings and symbols. 
+// The registry returns symbols using Symbol.for(key).
