a75e47199f402f39e2e3bbc9b1043b0ed8e13ba0
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-31 15:45:12 -0500

    global symbol registry

--- js-es6+/09-symbols/02-global-symbol-registry.js
+++ js-es6+/09-symbols/02-global-symbol-registry.js
@@ -1,3 +1,14 @@
 // ES6 has a global resource for creating symbols: the symbol registry. 
 // The symbol registry provides us with a one - to - one relationship between strings and symbols. 
 // The registry returns symbols using Symbol.for(key).
+
+// Symbol.for( key1 ) === Symbol.for( key2 ) whenever key1 === key2. 
+// This correspondance works even across service workers and iframes.
+let privateProperty1 = Symbol.for('firstName');
+let privateProperty2 = Symbol.for('firstName');
+
+myObject[privateProperty1] = 'Dave';
+myObject[privateProperty2] = 'Zsolt';
+
+console.log(myObject[privateProperty1]);
+// Zsolt
