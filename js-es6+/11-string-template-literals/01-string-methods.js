303003e0bb199502708cefc9d6f878afa7c46220
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-04 18:45:00 -0500

    string methods

---
+++ js-es6+/11-string-template-literals/01-string-methods.js
@@ -0,0 +1,4 @@
+// startsWith   s1.startsWith(s2)   true if and only if s1 starts with s2
+// endsWith   s1.endsWith(s2)   true if and only if s1 ends with s2
+// includes:   s1.includes(s2)   true if and only if s2 is a substring of s1
+// repeat   s.repeat(n)   replicates s n times, and joins them
