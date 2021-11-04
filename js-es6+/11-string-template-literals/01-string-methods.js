07758355bfc18f6c5d7f3102a1435774dd9cdaea
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-04 18:45:12 -0500

    string methods

--- js-es6+/11-string-template-literals/01-string-methods.js
+++ js-es6+/11-string-template-literals/01-string-methods.js
@@ -2,3 +2,14 @@
 // endsWith   s1.endsWith(s2)   true if and only if s1 ends with s2
 // includes:   s1.includes(s2)   true if and only if s2 is a substring of s1
 // repeat   s.repeat(n)   replicates s n times, and joins them
+
+console.log('Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz'.startsWith( 'Rindfleisch' ));
+//> true
+console.log('not good'.endsWith( 'good' ));
+//> true
+
+console.log('good or bad'.includes( ' or ' ));
+//> true
+
+console.log('ha'.repeat( 4 ));
+//> 'hahahaha'
