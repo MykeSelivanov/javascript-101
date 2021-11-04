060d6a82a21aac7e40169ce63d372a8c6c087fb2
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-03 21:50:45 -0500

    destructuring and DOM with for of loop

--- js-es6+/10-for-of-loop/02-destructuring-and-DOM-with-for-of.js
+++ js-es6+/10-for-of-loop/02-destructuring-and-DOM-with-for-of.js
@@ -8,3 +8,15 @@ for (let { source, destination } of flights) {
     console.log(source, destination);
 }
 
+// When you use destructuring, you can omit the fields you are not interested in.
+for (let { source } of flights) {
+    console.log(source);
+}
+
+// It is possible to use the for-of loop on a NodeList. 
+// The below snippet changes the default font color of each div on a website randomly. 
+let divs = document.querySelectorAll('div');
+for (let div of divs) {
+    let rand = Math.floor(Math.random() * 3);
+    div.style.color = ['#990000', '#009900', '#000099'][rand];
+}
