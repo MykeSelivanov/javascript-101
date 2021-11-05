bca3ae403153b1b2262197230a137fbcc3c3c946
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-05 16:51:27 -0500

    template literals

--- js-es6+/11-string-template-literals/03-template-literals.js
+++ js-es6+/11-string-template-literals/03-template-literals.js
@@ -8,3 +8,10 @@ let y = '555'; //<-notice '555'
 evaluatedTemplate = `${y} === 555 is ${y === 555}`;
 console.log(evaluatedTemplate);
 // evaluatedTemplate becomes "555 === 555 is false"
+
+// One practical application of template literals is the creation of DOM markup with static structure, and dynamic values.
+// Another application of template literals is to simplify writing strings that span multiple lines:
+`first
+second
+third
+fourth`
