39ff6c8a458dd5cbb4b796f75125c837aed73d59
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-07 13:26:35 -0600

    es6 sets

---
+++ js-es6+/12-sets-maps-their-weak-versions/es6-sets.js
@@ -0,0 +1,6 @@
+// A Set data structure in ES6 is an ORDERED list of unique elements. Here are some set functions and their usage:
+// Set - constructor - creates a set
+// add - method - adds elements to the set
+// size - property - check size of the set
+// has - method - checks if an element is a member of the set
+// delete - method - remove a value from a set
