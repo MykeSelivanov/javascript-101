2063e60b0ce45699c317437bfc3cb304c4d71942
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-10 17:59:41 -0600

    iterating maps

--- js-es6+/12-sets-maps-their-weak-versions/04-iterating-maps.js
+++ js-es6+/12-sets-maps-their-weak-versions/04-iterating-maps.js
@@ -13,3 +13,9 @@ for (let [key, value] of horses) {
 console.log('\nspread operators:');
 console.log([...horses]);
 //> [[8,"Chocolate"],[3,"Filippone"]]
+
+// Notice the following behavior:
+
+// - The destructuring operation in the for...of loop. Iterating a map results in a key-value pair.
+// - The spread operator creates an enumeration of key-value pairs. These key-value pairs can be used to build an array.
+// - The order of elements in the forEach method is value, key. The order in the for...of loop is key, value.
