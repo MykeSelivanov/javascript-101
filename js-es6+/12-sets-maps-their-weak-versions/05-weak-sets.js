a1f0c3208e9447744eee6b496573513f9bbc24b2
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-10 18:01:11 -0600

    weak sets

---
+++ js-es6+/12-sets-maps-their-weak-versions/05-weak-sets.js
@@ -0,0 +1,7 @@
+// Sets and maps hold a reference of their values. This means that the garbage collector won’t be able to collect the 
+// values in sets, and key-value pairs in maps to free some memory.
+
+// This is where weak sets and maps come into play. They only hold weak references to their values, allowing 
+// garbage collection of the values while they are members of a set or map.
+
+// Weak sets are similar to regular sets. The main difference is that their elements may disappear once they are garbage collected.
