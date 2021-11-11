eab0892b0bf98214c4d89148a8b5756c94346d66
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-11 17:44:43 -0600

    weak maps

---
+++ js-es6+/12-sets-maps-their-weak-versions/06-weak-maps.js
@@ -0,0 +1,2 @@
+// Weak maps have object keys, and arbitrary values. When all strong references to a key are removed, 
+// the key is garbage collected, and the key-value pair is removed from the weak map.
