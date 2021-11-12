c9535be25b87ba824abefded1e845dff05ab4391
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-12 17:03:31 -0600

    weak maps

--- js-es6+/12-sets-maps-their-weak-versions/06-weak-maps.js
+++ js-es6+/12-sets-maps-their-weak-versions/06-weak-maps.js
@@ -2,3 +2,15 @@
 // the key is garbage collected, and the key-value pair is removed from the weak map.
 
 // Only the keys of weak maps are weak. Values placed in a weak map have strong references in the map.
+
+let firstElement = { order: 1 }, secondElement = { order: 2 };
+let wm = new WeakMap();
+
+wm.set(firstElement, 1);
+wm.set(secondElement, {});
+
+console.log(wm.get(secondElement));
+//> {}
+
+delete secondElement;
+// secondElement is removed from the weak map
