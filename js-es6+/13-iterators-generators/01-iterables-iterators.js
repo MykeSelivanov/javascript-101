09e5b140a0860121742d1c781ec3c4fabc07ff74
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-14 16:13:59 -0600

    iterables iterators

--- js-es6+/13-iterators-generators/01-iterables-iterators.js
+++ js-es6+/13-iterators-generators/01-iterables-iterators.js
@@ -11,3 +11,18 @@ let iterableObject = {
 
 // An iterator object is a data structure that has a next method. When calling this method on the iterator, 
 // it returns the next element, and a boolean signaling whether we reached the end of the iteration.
+
+// Place this before iterableObject
+let iteratorObject = {
+    next() {
+        return {
+            done: true,
+            value: null
+        };
+    }
+};
+
+// The return value of the next function has two keys:
+// - done is treated as a boolean. When done is true, the iteration ends, and value is not considered in the iteration.
+// - value is the upcoming value of the iteration. It is considered in the iteration if and only if done is false. 
+// When done is true, value becomes the return value of the iterator.
