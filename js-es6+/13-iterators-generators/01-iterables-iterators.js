2f4eea7d0b57bb826214091bc8bb27a0484a0d48
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-14 16:12:06 -0600

    iterables iterators

--- js-es6+/13-iterators-generators/01-iterables-iterators.js
+++ js-es6+/13-iterators-generators/01-iterables-iterators.js
@@ -1,2 +1,6 @@
 // ES6 comes with the iterable protocol. The protocol defines the iterating behavior of JavaScript objects.
 // An iterable object has an iterator method with the key Symbol.iterator. This method returns an iterator object.
+
+let iterableObject = {
+    [Symbol.iterator]() { return iteratorObject; }
+};
