cd713b4d476ec7cfcd771206fbb315872ae149fb
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-14 16:13:14 -0600

    iterables iterators

--- js-es6+/13-iterators-generators/01-iterables-iterators.js
+++ js-es6+/13-iterators-generators/01-iterables-iterators.js
@@ -4,3 +4,10 @@
 let iterableObject = {
     [Symbol.iterator]() { return iteratorObject; }
 };
+
+// Symbol.iterator is a well-known symbol. We will now use Symbol.iterator to describe an iterable object. 
+// Note that we are using this construct for the sake of understanding how iterators work. 
+// Technically, you will hardly ever need Symbol.iterator in your code.
+
+// An iterator object is a data structure that has a next method. When calling this method on the iterator, 
+// it returns the next element, and a boolean signaling whether we reached the end of the iteration.
