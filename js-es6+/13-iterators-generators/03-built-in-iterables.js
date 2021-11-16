718232e25fe2722aa23d1b26aebc8850804d3b89
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-16 09:09:46 -0600

    built in iterables

--- js-es6+/13-iterators-generators/03-built-in-iterables.js
+++ js-es6+/13-iterators-generators/03-built-in-iterables.js
@@ -2,3 +2,23 @@
 // - Strings are iterables as arrays of 2 to 4-byte characters.
 // - DOM data structures are also iterables. If you want proof, just open a random website, and execute [...document.querySelectorAll('p')] in the console
 // - Maps and Sets are iterables.
+
+let message = 'ok';
+
+let stringIterator = message[Symbol.iterator]();
+let secondStringIterator = message[Symbol.iterator]();
+
+stringIterator.next();
+//> Object {value: "o", done: false}
+
+secondStringIterator.next();
+//> Object {value: "o", done: false}
+
+stringIterator.next();
+//> Object {value: "k", done: false}
+
+stringIterator.next();
+//> Object {value: undefined, done: true}
+
+secondStringIterator.next();
+//> Object {value: "k", done: false}
