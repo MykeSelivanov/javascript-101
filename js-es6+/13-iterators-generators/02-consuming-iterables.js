37b1bc34db727d8467bce312ccebd02483f385bb
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-16 09:06:21 -0600

    consuming iterables

---
+++ js-es6+/13-iterators-generators/02-consuming-iterables.js
@@ -0,0 +1,6 @@
+// Both the for-of loop and the spread operator can be used to perform iteration on an iterable object.
+for (let element of iterableObject) {
+    console.log(element);
+}
+
+console.log([...iterableObject]);
