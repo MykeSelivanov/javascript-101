60af341341f9cceee4d3c35990375fddecc6013a
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-16 09:08:08 -0600

    consuming iterables

--- js-es6+/13-iterators-generators/02-consuming-iterables.js
+++ js-es6+/13-iterators-generators/02-consuming-iterables.js
@@ -4,3 +4,25 @@ for (let element of iterableObject) {
 }
 
 console.log([...iterableObject]);
+
+let countdownIterator = {
+    countdown: 10,
+    next() {
+        this.countdown -= 1;
+        return {
+            done: this.countdown === 0,
+            value: this.countdown
+        };
+    }
+};
+
+let countdownIterable = {
+    [Symbol.iterator]() {
+        return Object.assign({}, countdownIterator)
+    }
+};
+
+console.log([...countdownIterable]);
+  //> [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+// Language constructs that consume iterable data are called data consumers.
