21af1d9c4d67ecf51b0fba22d7cf73c9cf31e49a
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-16 09:09:28 -0600

    built in iterables

---
+++ js-es6+/13-iterators-generators/03-built-in-iterables.js
@@ -0,0 +1,4 @@
+// - Arrays are iterables, and work well with the for-of loop.
+// - Strings are iterables as arrays of 2 to 4-byte characters.
+// - DOM data structures are also iterables. If you want proof, just open a random website, and execute [...document.querySelectorAll('p')] in the console
+// - Maps and Sets are iterables.
