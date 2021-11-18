30525c275db6cd7e4c4cb2917c5ac4e7869e44ba
MykeSelivanov <michael777selivanov@gmail.com>
2021-11-18 17:56:48 -0600

    iterables sets maps

--- js-es6+/13-iterators-generators/04-iterables-sets-maps.js
+++ js-es6+/13-iterators-generators/04-iterables-sets-maps.js
@@ -1,2 +1,28 @@
 // The entries method is defined on sets and maps. You can also use the keys and values method on a 
 // set or map to create an iterator/iterable of the keys or values. 
+
+let colors = new Set(['red', 'yellow', 'green']);
+let horses = new Map([
+    [5, 'QuickBucks'],
+    [8, 'Chocolate'],
+    [3, 'Filippone']
+]);
+
+console.log(colors.entries());
+//> SetIterator {["red", "red"], ["yellow", "yellow"], ["green", "green"]}
+
+console.log('\n')
+console.log(colors.keys());
+//> SetIterator {"red", "yellow", "green"}
+
+console.log(colors.values());
+//> SetIterator {"red", "yellow", "green"}
+
+console.log(horses.entries());
+//> MapIterator {[5, "QuickBucks"], [8, "Chocolate"], [3, "Filippone"]}
+
+console.log(horses.keys());
+//> MapIterator {5, 8, 3}
+
+console.log(horses.values());
+//> MapIterator {"QuickBucks", "Chocolate", "Filippone"}
